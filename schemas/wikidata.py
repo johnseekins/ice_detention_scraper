eb6219fbf1698e5ff3795dd0b6b78c39e5cb1316
John Seekins <john@robot-house.us>
2025-09-30 21:40:40 -0600

    more sub schemas
    Signed-off-by: John Seekins <john@robot-house.us>

---
+++ schemas/wikidata.py
@@ -0,0 +1,10 @@
+{
+    "$id": "schema:wikidata",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "Wikidata Facilities Details",
+    "type": "object",
+    "properties": {
+        "page_url": {"type": "string", "description": "Final URL retrieved from wikidata"},
+        "search_query": {"type": "string", "description": "All queries performed to find data"},
+    },
+}
